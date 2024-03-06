e9fe8d2c91e0fd0dd67b7df0c190f1a036cf0f97
Shilpajordan <Shilpa.nair@dci-student.org>
2024-03-06 17:03:29 +0100

    editing the contact Page

--- consult/website/views.py
+++ consult/website/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == 'POST':
+
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        #send email
+        send_mail(
+           message_name , #subject
+            message, #message
+            message_email, # from email
+            ['drcshaefer@gmail.com'],# to email
+            fail_silently=False,  # to throw up an error message if it fails
+        )
+
+        return render(request, 'contact.html', {'message_name': message_name})
+    
+    else:
+        #
+        return render(request, 'contact.html', {})
